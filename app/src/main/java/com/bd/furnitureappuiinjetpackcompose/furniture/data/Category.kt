2bead157b84f0635613710f5c29ee23b77501a60
subroto <subroto_mohonto@yahoo.com>
2024-06-04 22:17:40 +0600

    Category Data class created

--- app/src/main/java/com/bd/furnitureappuiinjetpackcompose/furniture/data/Category.kt
+++ app/src/main/java/com/bd/furnitureappuiinjetpackcompose/furniture/data/Category.kt
@@ -1,2 +1,11 @@
 package com.bd.furnitureappuiinjetpackcompose.furniture.data
 
+import androidx.annotation.DrawableRes
+import androidx.compose.ui.graphics.Color
+
+data class Category(
+    val id: Int,
+    val title: String,
+    @DrawableRes val image: Int,
+    val color: Color
+)
