4c32a88c266b0947273ebba41095f27e2eb578ce
subroto <subroto_mohonto@yahoo.com>
2024-06-07 01:53:01 +0600

    back button in detail  screen

--- app/src/main/java/com/bd/furnitureappuiinjetpackcompose/ui/screens/product_detail_screen.kt
+++ app/src/main/java/com/bd/furnitureappuiinjetpackcompose/ui/screens/product_detail_screen.kt
@@ -1,11 +1,27 @@
 package com.bd.furnitureappuiinjetpackcompose.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.bd.furnitureappuiinjetpackcompose.R
+import com.bd.furnitureappuiinjetpackcompose.ui.navigation.Home
 import com.bd.furnitureappuiinjetpackcompose.ui.theme.Background
 import com.bd.furnitureappuiinjetpackcompose.ui.theme.DarkGreen
 
@@ -13,35 +29,41 @@ import com.bd.furnitureappuiinjetpackcompose.ui.theme.DarkGreen
 fun ProductDetailScreen(
     navHostController: NavHostController
 ) {
-
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(DarkGreen)
-    ) {
-        /*Column(
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.product_four), contentDescription = "",
             modifier = Modifier
-                .fillMaxSize()
-                .padding(20.dp)
-        ) {
-            ProductHeader {
-                navHostController.navigateUp()
-            }
-            Spacer(modifier = Modifier.height(20.dp))
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
-                item {
-                    ShowProduct()
-                    ProductDescription()
-                }
-            }
+                .fillMaxWidth()
+                .height(250.dp),
+            contentScale = ContentScale.FillWidth
+        )
+        HeaderSection {
+            navHostController.navigateUp()
+        }
+    }
+
+}
+
+@Composable
+fun HeaderSection(
+    onBack: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+
+        IconButton(onClick = onBack) {
+            Icon(Icons.Default.ArrowBack, contentDescription = "", tint = Color.Black)
         }
 
-        ButtonComponent(
-            modifier = Modifier
-                .padding(20.dp)
-                .align(Alignment.BottomCenter)
-        )*/
-
+        Icon(
+            painter = painterResource(id = R.drawable.share),
+            contentDescription = "",
+            tint = Color.Unspecified
+        )
     }
 
 }
